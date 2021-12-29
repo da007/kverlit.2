f6ac92fc6afd8144b5a0a3371c260668a12b84a9
da007 <89918708+da007@users.noreply.github.com>
2021-12-29 17:17:25 +0300

    Delete App directory

--- App/Modules/js/map.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const site = {
-	'welcome': {name: 'welcome', path:'./Modules/html/welcome', ext: 'html'},
-	'login'  : {name: 'login', path:'./Modules/html/login', ext: 'html'},
-	'sing-up': {name: 'sing-up', path:'./Modules/html/sing-up', ext: 'html'},
-	'404'    : {name: '404', path:'./Modules/html/404', ext: 'html'},
-	'server' : {name: 'server', path: './server/...'}
-};
